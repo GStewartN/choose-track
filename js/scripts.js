456be7595b110fff829edfa1a6bac41162f88d02
GStewartN <stewart.nathang@gmail.com>
2017-06-02 13:59:04 -0700

    add #php and #many-tracks divs to html with corresponding style rules in css

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,40 @@
-var workStyle1 = $("input:radio[name=workstyle]:checked").val();
-var workStyle2 = $("input:radio[name=workstyle]:checked").val();
-var workStyle3 = $("input:radio[name=workstyle]:checked").val();
+//var workStyle1 = $("input:radio[name=workstyle]:checked").val();
+//var workStyle2 = $("input:radio[name=workstyle]:checked").val();
+//var workStyle3 = $("input:radio[name=workstyle]:checked").val();
 
-var creativityStyle1 = $("input:radio[name=creativitystyle]:checked").val();
-var creativityStyle2 = $("input:radio[name=creativitystyle]:checked").val();
-var creativityStyle3 = $("input:radio[name=creativitystyle]:checked").val();
+//var creativityStyle1 = $("input:radio[name=creativitystyle]:checked").val();
+//var creativityStyle2 = $("input:radio[name=creativitystyle]:checked").val();
+//var creativityStyle3 = $("input:radio[name=creativitystyle]:checked").val();
 
-var businessStyle1 = $("input:radio[name=businessstyle]:checked").val();
-var businessStyle2 = $("input:radio[name=businessstyle]:checked").val();
-var businessStyle3 = $("input:radio[name=businessstyle]:checked").val();
+//var businessStyle1 = $("input:radio[name=businessstyle]:checked").val();
+//var businessStyle2 = $("input:radio[name=businessstyle]:checked").val();
+//var businessStyle3 = $("input:radio[name=businessstyle]:checked").val();
 
-var visualize1 = $("input:radio[name=visualization]:checked").val();
-var visualize2 = $("input:radio[name=visualization]:checked").val();
+//var visualize1 = $("input:radio[name=visualization]:checked").val();
+//var visualize2 = $("input:radio[name=visualization]:checked").val();
 
-var designType1 = $("input:radio[name=designtype]:checked").val();
-var designType2 = $("input:radio[name=designtype]:checked").val();
+//var designType1 = $("input:radio[name=designtype]:checked").val();
+//var designType2 = $("input:radio[name=designtype]:checked").val();
 
 
 
 
 $(document).ready(function() {
   $("#submit").click(function(event) {
+    var creativityStyle2 = $("input:radio[name=creativitystyle]:checked").val();
+    var designType1 = $("input:radio[name=designtype]:checked").val();
+    var businessStyle1 = $("input:radio[name=businessstyle]:checked").val();
 
-
-
-    if (workStyle1 === "true" && creativityStyle2 === "true") {
+    if (creativityStyle2) {
       $("#ruby").show();
-    } else {
+    } else if (designType1) {
      $("#css").show();
-    }
+   } else if (businessStyle1) {
+     $("#php").show();
+   } else {
+     $("#many-tracks").show();
+   }
+
     event.preventDefault();
   });
 });
