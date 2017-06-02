84ad96aab5aa3c8017e29fdf8df9a03d7e0b67a3
GStewartN <stewart.nathang@gmail.com>
2017-06-02 10:54:46 -0700

    create rough layout of Business logic in scripts.js for testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
+var workStyle1 = $("input:radio[name=workstyle]:checked").val();
+var workStyle2 = $("input:radio[name=workstyle]:checked").val();
+var workStyle3 = $("input:radio[name=workstyle]:checked").val();
 
+var creativityStyle1 = $("input:radio[name=creativitystyle]:checked").val();
+var creativityStyle2 = $("input:radio[name=creativitystyle]:checked").val();
+var creativityStyle3 = $("input:radio[name=creativitystyle]:checked").val();
 
+var businessStyle1 = $("input:radio[name=businessstyle]:checked").val();
+var businessStyle2 = $("input:radio[name=businessstyle]:checked").val();
+var businessStyle3 = $("input:radio[name=businessstyle]:checked").val();
 
+var visualize1 = $("input:radio[name=visualization]:checked").val();
+var visualize2 = $("input:radio[name=visualization]:checked").val();
 
-
-
-
-
-
-
-
-
-
+var designType1 = $("input:radio[name=designtype]:checked").val();
+var designType2 = $("input:radio[name=designtype]:checked").val();
 
 
 
@@ -22,11 +26,11 @@ $(document).ready(function() {
 
     var result;
     if () {
-
+      result =
     } else if () {
-
+      result =
     } else if () {
-
+      result =
     }
     $("#output").text(result);
   });
