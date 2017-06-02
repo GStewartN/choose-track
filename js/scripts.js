6a284166ca83277a70110372ecc922c5f0622e82
GStewartN <stewart.nathang@gmail.com>
2017-06-02 10:23:02 -0700

    create rough layout of UI logic in scripts.js for testing

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+  $("div#radio").submit(function(event) {
+    event.preventDefault();
+
+    var result;
+    if () {
+
+    } else if () {
+
+    } else if () {
+
+    }
+    $("#output").text(result);
+  });
+});
