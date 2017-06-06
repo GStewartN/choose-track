f58c66c00bbb4a466f5002d12b747ec91c641102
GStewartN <stewart.nathang@gmail.com>
2017-06-05 20:26:46 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
   $("#submit").click(function(event) {
 
-    var workStyle1 = $("input:radio[name=workpublic]:checked").val();
-    var workStyle2 = $("input:radio[name=workcube]:checked").val();
-    var workStyle3 = $("input:radio[name=workboth]:checked").val();
+    var workStyle1 = $("input:radio[name=workstyle]:checked").val();
+    var workStyle2 = $("input:radio[name=workstyle]:checked").val();
+    var workStyle3 = $("input:radio[name=workstyle]:checked").val();
 
-    var creativityStyle1 = $("input:radio[name=creativitymobile]:checked").val();
-    var creativityStyle2 = $("input:radio[name=creativityapp]:checked").val();
-    var creativityStyle3 = $("input:radio[name=creativitysoftware]:checked").val();
+    var creativityStyle1 = $("input:radio[name=creativitystyle]:checked").val();
+    var creativityStyle2 = $("input:radio[name=creativitystyle]:checked").val();
+    var creativityStyle3 = $("input:radio[name=creativitystyle]:checked").val();
 
-    var businessStyle1 = $("input:radio[name=businesslarge]:checked").val();
-    var businessStyle2 = $("input:radio[name=businessmedium]:checked").val();
-    var businessStyle3 = $("input:radio[name=businessstartup]:checked").val();
+    var businessStyle1 = $("input:radio[name=businessstyle]:checked").val();
+    var businessStyle2 = $("input:radio[name=businessstyle]:checked").val();
+    var businessStyle3 = $("input:radio[name=businessstyle]:checked").val();
 
-    var visualize1 = $("input:radio[name=visualizationsee]:checked").val();
-    var visualize2 = $("input:radio[name=visualizationmake]:checked").val();
+    var visualize1 = $("input:radio[name=visualization]:checked").val();
+    var visualize2 = $("input:radio[name=visualization]:checked").val();
 
-    var designType1 = $("input:radio[name=designlook]:checked").val();
-    var designType2 = $("input:radio[name=designcreate]:checked").val();
+    var designType1 = $("input:radio[name=designstyle]:checked").val();
+    var designType2 = $("input:radio[name=designstyle]:checked").val();
 
-    if (workStyle3 && creativityStyle2 && businessStyle2) {
+    if (workStyle3 === "both" && creativityStyle2 === "interactive web apps" && businessStyle2 === "medium-sized business") {
       $("#ruby").show();
-    } else if (workStyle1 && visualize1 && designType1) {
+    } else if (workStyle1 === "public" && visualize1 === "see my work" && designType1 === "look nice") {
      $("#css").show();
-   } else if (workStyle2 && creativityStyle3 && businessStyle1) {
+   } else if (workStyle2 === "cubicle" && creativityStyle3 === "back-end software" && businessStyle1 === "large coorporation") {
      $("#php").show();
    } else {
      $("#many-tracks").show();
